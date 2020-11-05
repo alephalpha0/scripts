1f240b7143be72e529566351692d137868d9ffe0
alephalpha0 <alephalpha0@rootofpi.me>
2020-11-05 00:29:52 -0600

    Added license to the repo and scripts. GPL3. Made a snippet for easy access of certrain GPL3 functions.

---
+++ snippets/gpl3_interactive_blurb.sh
@@ -0,0 +1,23 @@
+gpl3Blurb() {
+	echo "    <program>  Copyright (C) <year>  <name of author>"
+	echo "    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'."
+	echo "    This is free software, and you are welcome to redistribute it"
+	echo "    under certain conditions; type `show c' for details."
+}
+
+gpl3Warranty() {
+	echo "THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY"
+	echo "APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT"
+	echo "HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT"
+	echo "WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT"
+	echo "NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS"
+	echo "FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND"
+	echo "PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE"
+	echo "DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR"
+	echo "OR CORRECTION."
+}
+
+gpl3Conditions() {
+	echo "For a full text of the Terms and Conditions for redistribution"
+	echo "please refer to https://www.gnu.org/licenses/gpl-3.0.en.html"
+}

3ecaa5b884e733e10b7c8e1551229599e47f20bf
alephalpha0 <alephalpha0@tutanota.com>
2021-02-21 13:41:53 -0600

    fileDown now  uses youtube-dl correctly, outputs to Artist-Title.mp3 AND tags the mp3! I did it!

--- fileDown.sh
+++ fileDown.sh
@@ -1,13 +1,42 @@
-#!/bin/sh
+#!/bin/bash
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# NAME: fileDowmn ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# DESC: Downloads files passed to it, tagging mp3s as needed.  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# CREATED: 2021-01-10 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# MODIFIED: 2021-02-21 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# CONTACT: alephalpha0@tutanota.com && https://github.com/alephalpha0 ;;;;;;;;;;;;;;;;;;;;;;;;#
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# Many thanks to those who have shared and discussed their dotfiles/source code/scripts on    #
+# blogs, stackoverflow, dev.to, gitlab/github, and on any open forum for ideas on the world   #
+# wide web. I am continually learning and finding new things because of you.                  #
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# fileDown - Downloads URLs passed to it, using wget and youtube-dl. Tags MP3s TOO!.
+# Copyright (C) 20201  alephalpha0 [alephalpha0@tutanota.com]
+
+# This program is free software: you can redistribute it and/or modify it under the terms of 
+# the GNU General Public License as published by the Free Software Foundation, either version 
+# 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. 
+# If not, see <https://www.gnu.org/licenses/>.
 
 ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-# lovely variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# LOVELY VARIABLES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# (String) The URL passed to the script ;;;;;;;;;;;;;;;;;;;;;;
 file2dl="${1}"
+# (String) The local directory to download into. ;;;;;;;;;;;;;
 directory=""
+# (Int) The User Selected Download Method. ;;;;;;;;;;;;;;;;;;;
 dlmethod=""
+# (String) The createdMP3 file ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+mp3File=""
 
-#youtube-dl="/data/data/com.termux/files/usr/bin/youtube-dl"
-#youtube-dl="/data/data/com.termux/files/home/bin/youtube-dl"
 
 printLine() {
 	echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -28,8 +57,10 @@ echo "        ░█▀▀░░█░░█░░░█▀▀░█░█░█
 echo "        ░▀░░░▀▀▀░▀▀▀░▀▀▀░▀▀░░▀▀▀░▀░▀░▀░▀░1 "
 printLine
 echo "::::::::: What directory to download to? :::::::::::::::"
+printLine
 read -r directory
-
+printLine
+echo "Downloading into:" "${directory}"
 if [ -d "${HOME}/${directory}" ]; then
   cd "${HOME}/${directory}" || exit $?
 else
@@ -51,23 +82,76 @@ done
 case "${dlmethod}" in
   1)
     printLine
-    echo "Downloading file via wget into ~/${directory}"
+    echo "::::::::: Downloading file via wget into ~/${directory}"
     wget "${file2dl}"
     exit 0
   ;;
   2)
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    # MP3 RENAMING AND TAGGING VARIABLES ;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    # (String) Date this ripping occured. ;;;;;;;;;;;;;;;;;;;;;;;;
+    ripDate=""
+    # (String) Artist Name ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mp3Artist=""
+    # (String) Song Title ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mp3Title=""
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    # Get the current date for the rip date.
+    printf -v ripDate '%(%B %d, %Y)T' -1
+
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    # GETTING MP3 ARTIST AND TITLE FOR TAGGING ;;;;;;;;;;;;;;;;;;;
     printLine
-    echo "Downloading youtube[music] link into mp3"
-    youtube-dl --restrict-filenames -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -c "${file2dl}" -o "%(title)s.%(ext)s"
-    exit 0
+    echo "::::::::: Artist name? ::::::::::::::::::::::::::::::::::"
+    printLine
+    read -r mp3Artist
+    while [ -z "${mp3Artist}" ]; do
+      echo "(:::!:::) Enter a valid string. ::::::::::::::::::::::::"
+      read -r mp3Artist
+    done
+    printLine
+    echo "::::::::: Song title? :::::::::::::::::::::::::::::::::::"
+    printLine
+    read -r mp3Title
+    while [ -z "${mp3Title}" ]; do
+      echo "(:::!:::) Enter a valid string. ::::::::::::::::::::::::"
+      read -r mp3Title
+    done
+    ## END USER INPUT FOR TAGGING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    # DOWNLOAD FILE AND ENCODE INTO MP3 ;;;;;;;;;;;;;;;;;;;;;;;;;;
+    echo "Downloading youtube[music] link into mp3 :::::::::::::"
+    youtube-dl --restrict-filenames -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -c "${file2dl}" -o "${mp3Artist}"'-'"${mp3Title}"'.%(ext)s'
+    mp3File="${mp3Artist}"'-'"${mp3Title}"'.mp3'
+    printLine
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    # MP3 TAGGING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    echo "Writing default id3 tags. ::::::::::::::::::::::::::::"
+    
+    mid3v2 -ve -a "${mp3Artist}" -t "${mp3Title}" -A 'Youtube-DL Rips' -c 'Ripped from Youtube via youtube-dl and encoded with ffmpeg on '"${ripDate}"' by alephalpha0 (alephalpha0@tutanota.com). Mors certa; vita incerta.' "${mp3File}"
+    
+
+    # END MP3 TAGGING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    #exit 0
   ;;
   3)
     printLine
-    echo "Downloading youtube link into mp4"
+    echo "Downloading youtube link into mp4 ::::::::::::::::::::"
     youtube-dl --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "${file2dl}"
   ;;
   *)
-    echo "What the actual fuck did you do man?!?!"
+    echo "What the actual fuck did you do man?!?! ::::::::::::::"
     exit 1
   ;;
 esac
--- newlog.sh
+++ newlog.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# NAME: newlog ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# DESC: Automatic creation of frontmattered MD files for the masses. ;;;;;;;;;;;;;;;;;;;;;;;;;#
+# CREATED: 2021-02-10 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# MODIFIED: 2021-02-20 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# CONTACT: alephalpha0@tutanota.com && https://github.com/alephalpha0 ;;;;;;;;;;;;;;;;;;;;;;;;#
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# Many thanks to those who have shared and discussed their dotfiles/source code/scripts on    #
+# blogs, stackoverflow, dev.to, gitlab/github, and on any open forum for ideas on the world   #
+# wide web. I am continually learning and finding new things because of you.                  #
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;#
+# newlog - generate a frontmattered new log file.
+# Copyright (C) 20201  alephalpha0 [alephalpha0@tutanota.com]
+
+# This program is free software: you can redistribute it and/or modify it under the terms of 
+# the GNU General Public License as published by the Free Software Foundation, either version 
+# 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. 
+# If not, see <https://www.gnu.org/licenses/>.
+
 
 ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-# lovely variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-title=""
-fulltitle=""
-yymmdd=""
-metadate=""
+# LOVELY VARIABLES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# (String) Will be the new log entry's Date. ;;;;;;;;;;;;;;;;;
+logDate=""
+# (String) Newlog entry's date formatted for it's metadata. ;;
+metaDate=""
+# (String) Will be the new log entry's title. ;;;;;;;;;;;;;;;;
+logTitle=""
+# (String) Combines $logDate + - + $logTitle + .md ;;;;;;;;;;;
+fullTitle=""
+
 
 EDITOR="/data/data/com.termux/files/usr/bin/micro"
 
 ##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 # the logic train of the script cho cho ;;;;;;;;;;;;;;;;;;;;;;
+##;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 if [ -d /data/data/com.termux/files/home/writing ]; then
   cd /data/data/com.termux/files/home/writing || exit $?
   echo "  ▗▄ ▗▖          ▗▖"
@@ -21,18 +54,19 @@ if [ -d /data/data/com.termux/files/home/writing ]; then
   echo "  ▐▌ █▌▝█▄▄▌▝█ █▘▐▙▄▄▖▝█▄█▘▝█▄█▌"
   echo "  ▝▘ ▀▘ ▝▀▀  ▀ ▀ ▝▀▀▀▘ ▝▀▘  ▞▀▐▌"
   echo "                            ▜█▛▘"
-  echo "New blog's title?"
-  read title
-  printf -v yymmdd '%(%Y-%m-%d)T' -1
-  printf -v metadate '%(%H:%M)T' -1
-  fulltitle="$yymmdd"-"$title".md
-  touch "${fulltitle}"
-  echo "Title:" >> "${fulltitle}"
-  echo "Date:" $yymmdd $metadate >> "${fulltitle}"
-  echo "Category: Blog" >> "${fulltitle}"
-  echo "Tags:" >> "${fulltitle}"
-  echo "Summary:" >> "${fulltitle}"
-  echo "Location:" >> "${fulltitle}"
-  echo "Status: Published" >> "${fulltitle}"
-  $EDITOR "${fulltitle}"
+  echo "New blog's title? No whitespace, use -."
+  read logTitle
+  printf -v logDate '%(%Y-%m-%d)T' -1
+  printf -v metaDate '%(%Y-%m-%d %H:%M)T' -1
+  fullTitle="$logDate"-"$logTitle".md
+  touch "${fullTitle}"
+  echo "Title:" >> "${fullTitle}"
+  echo "Date:" $metaDate >> "${fullTitle}"
+  echo "Category: Blog" >> "${fullTitle}"
+  echo "Tags:" >> "${fullTitle}"
+  echo "Summary:" >> "${fullTitle}"
+  echo "Location:" >> "${fullTitle}"
+  echo "Status: Published" >> "${fullTitle}"
+  echo "" >> "${fullTitle}"
+  $EDITOR "${fullTitle}"
 fi

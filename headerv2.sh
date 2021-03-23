4c9525c8b3c859a205bca6326d94df17631c8eeb
alephalpha0 <alephalpha0@tutanota.com>
2021-03-22 21:37:01 -0500

    Various header files

---
+++ headerv2.sh
@@ -0,0 +1,38 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#      SCRIPT_NAME_FULL      ||      CUTE_TAG_LINE          #
+#    created:202X-XX-XX      ||     modified:202X-XX-XX     #
+# product of alephalpha0's mind || alephalpha0@tutanota.com #
+# \\\\\\\\\\\\\\\\\ A ROOTOFPI PRODUCTION ///////////////// #
+#  http://github.com/alephalpha0       http://rootofpi.me   #
+#########################         ###########################
+##################                       ####################
+# Many thanks to those who have shared and discussed their  #
+# dotfiles/source code/scripts on blogs, stackoverflow,     #
+# dev.to, gitlab / github, and any open forum for ideas on  #
+# the world wide web. I am continually learning and finding #
+# new things because of you.                                #
+#############################################################
+#############################################################
+# <one line to give the program's name and a brief idea of what it does.>
+# Copyright (C) 2020X  alephalpha0 [alephalpha0@tutanota.com]
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# --------------------------------------------------------- #
+# FunctionName ()                                           #
+# Description of function use. What it does.                #
+# Parameters: $param1 $param2 $param3 {...}                 #
+# Returns: 0 on success, $E_ERRORNUM on failure.            #
+# --------------------------------------------------------- #
--- snippets/header.sh
+++ snippets/header.sh
@@ -1,33 +1,25 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-#      SCRIPT_NAME_FULL      ||      CUTE_TAG_LINE          #
-#    created:202X-XX-XX      ||     modified:202X-XX-XX     #
-# product of alephalpha0's mind || alephalpha0@tutanota.com #
-# \\\\\\\\\\\\\\\\\ A ROOTOFPI PRODUCTION ///////////////// #
-#  http://github.com/alephalpha0       http://rootofpi.me   #
-#########################         ###########################
-##################                       ####################
-# Many thanks to those who have shared and discussed their  #
-# dotfiles/source code/scripts on blogs, stackoverflow,     #
-# dev.to, gitlab / github, and any open forum for ideas on  #
-# the world wide web. I am continually learning and finding #
-# new things because of you.                                #
-#############################################################
-#############################################################
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# the name of the program @ 2021 alephalpha0 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# alephalpha0@tutanota.com Mors certa; vita incerta. ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# the purpose of the program. small blurb. ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# still blurbing? ok keep blurbing. ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# CREATED: YYYY-DD-MM _ MODIFIED: YYYY-DD-MM ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+# CHANGELOG: ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 # <one line to give the program's name and a brief idea of what it does.>
 # Copyright (C) 2020X  alephalpha0 [alephalpha0@tutanota.com]
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+# General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with this program.  
+# If not, see <https://www.gnu.org/licenses/>.
 
 
 # --------------------------------------------------------- #
